614d4507d67e48faaf093033cf95b4cd3f40974f
Aigerim Assylkhanova <assylkhanova0@gmail.com>
2021-02-11 19:42:28 +0300

    Aim_circle methods

--- source/map_objects.cpp
+++ source/map_objects.cpp
@@ -16,3 +16,22 @@ float &USO::Map_object::get_x_coord() {
 float &USO::Map_object::get_y_coord() {
     return y_pos;
 }
+
+void USO::Aim_circle::change_state() {
+    active_circle_radius -= active_circle_shift;
+}
+
+bool USO::Aim_circle::check_event(float x, float y) {
+    if ((x - x_pos) * (x - x_pos) +
+            (y - y_pos) * (y - y_pos) <= beat_radius) {
+        return true;
+    }
+    return false;
+}
+
+void USO::Aim_circle::draw(sf::RenderWindow window) {
+    sf::CircleShape circle(active_circle_radius);
+    circle.setPosition(x_pos, y_pos);
+    circle.setFillColor(sf::Color(230, 155, 230));
+    window.draw(circle);
+}
