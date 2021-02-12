95dd2281716cbe0dbffacb0b5bb4525242270d3b
rmzs <rmzs0711@yandex.ru>
2021-02-12 15:55:37 +0600

    little style fixes

--- include/map_master.h
+++ include/map_master.h
@@ -1,35 +1,35 @@
 #ifndef USO_MAP_MASTER_H
 #define USO_MAP_MASTER_H
 
-#include <list>
-#include <memory>
-#include <utility>
-#include <vector>
 #include "SFML/Graphics.hpp"
 #include "base_logic.h"
 #include "map_objects.h"
 #include "maps.h"
+#include <list>
+#include <memory>
+#include <utility>
+#include <vector>
 
 namespace USO {
 
 struct Map_choice_menu {
 private:
-    std::vector<std::unique_ptr<USO::Map>> map_pool;
+  std::vector<std::unique_ptr<USO::Map>> map_pool;
+
 public:
-    explicit Map_choice_menu(std::string& saved_maps_names_list)
-        : map_pool({}) {
-        //TODO читается файл по заданному адресу и потом по адресам ищет карты
-    }
+  explicit Map_choice_menu(std::string &saved_maps_names_list) : map_pool({}) {
+    // TODO читается файл по заданному адресу и потом по адресам ищет карты
+  }
 };
 
 struct Field {
 private:
-    std::list<std::shared_ptr<Map_object>> field_objects;
+  std::list<std::shared_ptr<Map_object>> field_objects;
 
 public:
-    void push(std::vector<std::shared_ptr<Map_object>>::iterator &map_object_it,
-              sf::Time);
+  void push(std::vector<std::shared_ptr<Map_object>>::iterator &map_object_it,
+            sf::Time);
 };
 
-}  // namespace USO
-#endif  // USO_MAP_MASTER_H
+} // namespace USO
+#endif // USO_MAP_MASTER_H
